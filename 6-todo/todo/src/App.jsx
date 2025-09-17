336956691219d32f24c18440c8c73409d9d6d450
yabsed <yabsed@gmail.com>
2025-09-18 03:17:10 +0900

    Begin Todo

---
+++ 6-todo/todo/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+    </>
+  );
+
+  //adsfadsf
+}
+
+export default App;
