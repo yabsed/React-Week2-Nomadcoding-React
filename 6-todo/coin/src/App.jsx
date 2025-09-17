2b4c9d65708abb43a9ea0bb0ab26f8f0d19ed072
yabsed <yabsed@gmail.com>
2025-09-18 04:04:15 +0900

    Clear

--- 6-todo/coin/src/App.jsx
+++ 6-todo/coin/src/App.jsx
@@ -1,34 +1,10 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-
-  useEffect(() => {
-    console.log("Use API");
-
-    fetch("https://api.coinpaprika.com/v1/tickers").then((response) =>
-      response.json().then((json) => {
-        setCoins(json);
-        setLoading(false);
-      })
-    );
-  }, []);
 
   return (
     <>
-      <h1>The Coins! {loading ? null : coins.length} </h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.symbol}: ${coin.quotes.USD.price.toFixed(2)}
-            </option>
-          ))}
-        </select>
-      )}
+      
     </>
   );
 }
