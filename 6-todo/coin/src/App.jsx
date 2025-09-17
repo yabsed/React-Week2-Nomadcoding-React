fca50c8513b8823fb849c866d1b3e074cbe55995
yabsed <yabsed@gmail.com>
2025-09-18 04:19:26 +0900

    lazy

--- 6-todo/coin/src/App.jsx
+++ 6-todo/coin/src/App.jsx
@@ -5,10 +5,9 @@ function App() {
   const [movies, setMovies] = useState([]);
 
   const getMovies = async () => {
-    const response = await fetch(
+    const json = await (await fetch(
       "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-    );
-    const json = await response.json();
+    )).json();
     setMovies(json.data.movies);
     setLoading(false);
   };
