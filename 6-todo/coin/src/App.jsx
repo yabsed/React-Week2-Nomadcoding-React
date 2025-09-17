21e5fa3e17e19a6eb9c41e85a9aa116e8c7d8c91
yabsed <yabsed@gmail.com>
2025-09-18 04:14:33 +0900

    Regreats

--- 6-todo/coin/src/App.jsx
+++ 6-todo/coin/src/App.jsx
@@ -1,12 +1,23 @@
 import { useState, useEffect } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
 
-  return (
-    <>
-      
-    </>
-  );
+  useEffect(() => {
+    fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+    ).then((response) =>
+      response.json().then((json) => {
+        setMovies(json.data.movies);
+      })
+    );
+    setLoading(false);
+  }, []);
+
+  console.log(movies);
+
+  return <>{loading ? <h1>Loading...</h1> : null}</>;
 }
 
 export default App;
