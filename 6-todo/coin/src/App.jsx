c04878f39096b7dcb3737577b0b2240e9e51ab40
yabsed <yabsed@gmail.com>
2025-09-18 11:57:50 +0900

    adsfjadsf

--- 6-todo/coin/src/App.jsx
+++ 6-todo/coin/src/App.jsx
@@ -1,42 +1,17 @@
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 import { useState, useEffect } from "react";
-import Movie from "./Movie.jsx";
+
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=${9}&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
   return (
-    <>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-            />
-          ))}
-        </div>
-      )}
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+      </Routes>
+    </Router>
   );
 }
 
--- 6-todo/coin/src/components/Movie.jsx
+++ 6-todo/coin/src/components/Movie.jsx
@@ -1,10 +1,13 @@
-import {PropTypes} from "prop-types"
+import { PropTypes } from "prop-types";
+import { Link } from "react-router-dom";
 
-function Movie({coverImg, title, summary, genres}) {
+function Movie({ coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to="/movie">{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
@@ -16,9 +19,9 @@ function Movie({coverImg, title, summary, genres}) {
 }
 
 Movie.propTypes = {
-  coverImg : PropTypes.string.isRequired, 
+  coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
-}
+};
 export default Movie;
