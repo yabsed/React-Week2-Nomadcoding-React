22c6345ea6a8b1e85ed95522b03082e731a2d89c
yabsed <yabsed@gmail.com>
2025-09-18 11:30:50 +0900

    module

--- 6-todo/coin/src/App.jsx
+++ 6-todo/coin/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Movie from "./Movie.jsx";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -23,20 +24,17 @@ function App() {
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <ul>
+        <div>
           {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image}/>
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
           ))}
-        </ul>
+        </div>
       )}
     </>
   );
