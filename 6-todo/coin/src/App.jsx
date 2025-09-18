d505f59ccb14852cb2886cf47401d8bf5c1aeb07
yabsed <yabsed@gmail.com>
2025-09-18 18:07:30 +0900

    score

--- 6-todo/coin/src/App.jsx
+++ 6-todo/coin/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movie" element={<Detail />} />
+        <Route path="/movie/:id" element={<Detail />} />
       </Routes>
     </Router>
   );
